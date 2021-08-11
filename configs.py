e5c74f79b5de3a31c919f66cc9543d75cd2a87b3
Good-Boys-Exe <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-08-11 08:46:32 +0700

    Update configs.py

--- configs.py
+++ configs.py
@@ -50,7 +50,7 @@ Juga ingat bahwa pengembang akan Menghapus Konten Dewasa dari Database. Jadi leb
 [Donate Now](DONATE_LINK)
 """
 	HOME_TEXT = f"""
-Hi, [{}](tg://user?id={})\n\nIni Permanen **{BOT_NAME}**.
+Hi, {}](tg://user?id={})\n\nIni Permanen **{BOT_NAME}**.
 
 Kirimi saya file apa pun, saya akan memberi Anda Tautan yang Dapat Dibagikan secara permanen. Saya Mendukung Saluran Juga! Centang Tombol **About Bot**.
-"""
+""",
